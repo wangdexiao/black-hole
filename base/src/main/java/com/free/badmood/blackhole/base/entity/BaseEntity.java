0ad23b305174384bd41bd9dee77af99c6a52f126
wangdexiao <wadexi2207@163.com>
2021-03-08 13:36:33 +0800

    数据库表自动填充时间格式修改， 上拉，下拉相关图片上传

--- base/src/main/java/com/free/badmood/blackhole/base/entity/BaseEntity.java
+++ base/src/main/java/com/free/badmood/blackhole/base/entity/BaseEntity.java
@@ -1,13 +1,17 @@
 package com.free.badmood.blackhole.base.entity;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 public abstract class BaseEntity implements Serializable {
@@ -16,10 +20,20 @@ public abstract class BaseEntity implements Serializable {
 
     @TableId(value = "id")
     private long id;
+
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd")//页面写入数据库时格式化
+//    @JSONField(format="yyyy-MM-dd HH:mm:ss")//数据库导出页面时json格式化
     @TableField(value = "create_time",fill = FieldFill.INSERT)
-    private LocalDateTime createTime;
+    private Date createTime;
+
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")//页面写入数据库时格式化
+//    @JSONField(format="yyyy-MM-dd HH:mm:ss")//数据库导出页面时json格式化
     @TableField(value = "update_time",fill = FieldFill.INSERT_UPDATE)
-    private LocalDateTime updateTime;
+    private Date updateTime;
+
+
     @TableLogic(/*value = "1",delval = "0"*/)
     private int del;
 
