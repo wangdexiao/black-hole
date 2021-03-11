a888bc7dc941d3d6ea235e18bd66f11d946afa85
wangdexiao <wadexi2207@163.com>
2021-03-11 10:07:19 +0800

    打包配置更改

---
+++ cos/src/main/java/com/free/badmood/blackhole/cos/config/CosConfig.java
@@ -0,0 +1,9 @@
+package com.free.badmood.blackhole.cos.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.free.badmood.blackhole.cos.web")
+public class CosConfig {
+}
