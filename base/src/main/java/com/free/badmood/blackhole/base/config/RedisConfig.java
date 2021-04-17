65464f06489328e4d3fe3c8836bf474656e17357
wangdexiao <wadexi2207@163.com>
2021-04-17 16:18:36 +0800

    redis 设置了密码没有验证问题修复1

--- base/src/main/java/com/free/badmood/blackhole/base/config/RedisConfig.java
+++ base/src/main/java/com/free/badmood/blackhole/base/config/RedisConfig.java
@@ -3,12 +3,14 @@ package com.free.badmood.blackhole.base.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -18,6 +20,12 @@ import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis")
+    public JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
 
 
     @Bean(name = "redisTemplate")
