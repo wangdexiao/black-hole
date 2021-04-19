b55cd6b5ecbb7bbecacfad3ca8f339b8215c2a5b
wangdexiao <wadexi2207@163.com>
2021-04-19 08:52:28 +0800

    redis 修改配置排查

--- black-hole-app/src/main/java/com/free/badmood/blackhole/config/redisconfig/Redisconfig.java
+++ black-hole-app/src/main/java/com/free/badmood/blackhole/config/redisconfig/Redisconfig.java
@@ -1,9 +1,20 @@
 package com.free.badmood.blackhole.config.redisconfig;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
@@ -20,5 +31,103 @@ public class Redisconfig {
     @ConfigurationProperties(prefix="spring.redis.jedis.pool")
     public JedisPoolConfig jedisPoolConfig() {
         return new JedisPoolConfig();
+    }
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        // 创建RedisTemplate<String, Object>对象
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        // 配置连接工厂
+        template.setConnectionFactory(factory);
+        // 定义Jackson2JsonRedisSerializer序列化对象
+        Jackson2JsonRedisSerializer<Object> jacksonSeial = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会报异常
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jacksonSeial.setObjectMapper(om);
+        StringRedisSerializer stringSerial = new StringRedisSerializer();
+        // redis key 序列化方式使用stringSerial
+        template.setKeySerializer(stringSerial);
+        // redis value 序列化方式使用jackson
+        template.setValueSerializer(jacksonSeial);
+        // redis hash key 序列化方式使用stringSerial
+        template.setHashKeySerializer(stringSerial);
+        // redis hash value 序列化方式使用jackson
+        template.setHashValueSerializer(jacksonSeial);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+
+    /**
+     * redis string
+     */
+    @Bean
+    public ValueOperations<String, Object> valueOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForValue();
+    }
+
+    /**
+     * redis hash
+     */
+    @Bean
+    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForHash();
+    }
+
+    /**
+     * redis list
+     */
+    @Bean
+    public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForList();
+    }
+
+    /**
+     * redis set
+     */
+    @Bean
+    public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForSet();
+    }
+
+    /**
+     * redis zset
+     */
+    @Bean
+    public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForZSet();
+    }
+
+
+    //    @Cacheabl、@CachePut、@CacheEvict的使用 见同目录RedisCacheUserService.java
+    @Bean
+    public CacheManager cacheManager(RedisTemplate<String, Object> redisTemplate) {
+        RedisCacheConfiguration defaultCacheConfiguration =
+                RedisCacheConfiguration
+                        .defaultCacheConfig()
+                        // 设置key为String
+                        .serializeKeysWith(RedisSerializationContext.SerializationPair
+                                .fromSerializer(redisTemplate.getStringSerializer()))
+                        // 设置value 为自动转Json的Object
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair
+                                .fromSerializer(redisTemplate.getValueSerializer()))
+                        // 不缓存null
+                        .disableCachingNullValues()
+                // 缓存数据保存1小时
+//                        .entryTtl(Duration.ofHours(1))
+                ;
+        RedisCacheManager redisCacheManager =
+                RedisCacheManager.RedisCacheManagerBuilder
+                        // Redis 连接工厂
+                        .fromConnectionFactory(redisTemplate.getConnectionFactory())
+                        // 缓存配置
+                        .cacheDefaults(defaultCacheConfiguration)
+                        // 配置同步修改或删除 put/evict
+                        .transactionAware()
+                        .build();
+        return redisCacheManager;
     }
 }
